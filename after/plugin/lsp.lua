6388f61c72c473bcdb858626e58db47d1d17b9e1
maxcaplan <maxacaplan@gmail.com>
2023-03-04 15:59:33 -0400

    Added LSP formating keymap and auto format

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,11 +1,23 @@
 local lsp = require('lsp-zero').preset({
-  name = 'minimal',
-  set_lsp_keymaps = true,
-  manage_nvim_cmp = true,
-  suggest_lsp_servers = false,
+	name = 'minimal',
+	set_lsp_keymaps = true,
+	manage_nvim_cmp = true,
+	suggest_lsp_servers = false,
 })
 
 -- (Optional) Configure lua language server for neovim
 lsp.nvim_workspace()
 
 lsp.setup()
+
+-- File formatting
+vim.keymap.set("n", "<leader>f", vim.cmd.LspZeroFormat)
+
+local group = vim.api.nvim_create_augroup("AutoFormat", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre", {
+	buffer = bufnr,
+	callback = function()
+		vim.lsp.buf.format()
+	end,
+	group = group
+})
