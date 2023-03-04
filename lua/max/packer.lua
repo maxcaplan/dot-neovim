fc0a08e82b3f1218d6b0b137973aff1989729cfc
maxcaplan <maxacaplan@gmail.com>
2023-03-04 16:08:00 -0400

    Organized plugins in packer.lua

--- lua/max/packer.lua
+++ lua/max/packer.lua
@@ -1,82 +1,87 @@
 -- Auto install packer
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 -- Install plugins
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim' 
+	use 'wbthomason/packer.nvim'
 
-  use {
-	  'glepnir/dashboard-nvim',
-	  config = function() require('max/dashboard') end,
-	  event = 'VimEnter'
-  }
+	-- Dashboard plugin for nvim startup
+	use {
+		'glepnir/dashboard-nvim',
+		config = function() require('max/dashboard') end,
+		event = 'VimEnter'
+	}
 
-  use {
-	  'nvim-lualine/lualine.nvim',
-	  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
+	-- Color Schemes
+	use {
+		"catppuccin/nvim",
+		as = "catppuccin",
+		config = function()
+			vim.cmd('colorscheme catppuccin')
+		end
+	}
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+	-- Interface Plugins
+	use("kyazdani42/nvim-web-devicons")
 
-  use { 
-	  "catppuccin/nvim", 
-	  as = "catppuccin", 
-	  config = function()
-		  vim.cmd('colorscheme catppuccin')
-	  end 
-  }
+	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
-  use("kyazdani42/nvim-web-devicons")
-  
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+	}
 
-  use('mbbill/undotree')
+	use('karb94/neoscroll.nvim')
 
-  use('tpope/vim-fugitive')
+	-- Productivity Plugns
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		requires = { { 'nvim-lua/plenary.nvim' } }
+	}
 
-  use('nvim-tree/nvim-tree.lua')
+	use('mbbill/undotree')
 
-  use('karb94/neoscroll.nvim')
+	use('tpope/vim-fugitive')
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v1.x',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},             -- Required
-		  {'williamboman/mason.nvim'},           -- Optional
-		  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+	use('nvim-tree/nvim-tree.lua')
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},         -- Required
-		  {'hrsh7th/cmp-nvim-lsp'},     -- Required
-		  {'hrsh7th/cmp-buffer'},       -- Optional
-		  {'hrsh7th/cmp-path'},         -- Optional
-		  {'saadparwaiz1/cmp_luasnip'}, -- Optional
-		  {'hrsh7th/cmp-nvim-lua'},     -- Optional
+	-- LSP Plugins
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v1.x',
+		requires = {
+			-- LSP Support
+			{ 'neovim/nvim-lspconfig' }, -- Required
+			{ 'williamboman/mason.nvim' }, -- Optional
+			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},             -- Required
-		  {'rafamadriz/friendly-snippets'}, -- Optional
-	  }
-  }
+			-- Autocompletion
+			{ 'hrsh7th/nvim-cmp' }, -- Required
+			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
+			{ 'hrsh7th/cmp-buffer' }, -- Optional
+			{ 'hrsh7th/cmp-path' }, -- Optional
+			{ 'saadparwaiz1/cmp_luasnip' }, -- Optional
+			{ 'hrsh7th/cmp-nvim-lua' }, -- Optional
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+			-- Snippets
+			{ 'L3MON4D3/LuaSnip' }, -- Required
+			{ 'rafamadriz/friendly-snippets' }, -- Optional
+		}
+	}
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
