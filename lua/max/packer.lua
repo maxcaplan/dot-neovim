a6dc21fb40305bff16eafeeeb92883a3f18ed283
maxcaplan <maxacaplan@gmail.com>
2023-03-04 14:12:17 -0400

    Added lauline plugin

--- lua/max/packer.lua
+++ lua/max/packer.lua
@@ -23,6 +23,11 @@ return require('packer').startup(function(use)
   }
 
   use {
+	  'nvim-lualine/lualine.nvim',
+	  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
+  use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
